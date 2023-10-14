d114230746c17af41c3c3786b5d1f4649652b7f9
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-14 15:14:27 +0200

    test ably

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -12,6 +12,15 @@ class Room extends Component {
         }
     }
 
+    async onTestClick(){
+        const Ably = require('ably');
+        const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
+        await ably.connection.once('connected');
+        console.log('Connected to Ably!');
+        const channel = ably.channels.get('test');
+        channel.publish('greeting', 'hello');
+    }
+
     static getDerivedStateFromProps(props, state) {
         return {
             room: props.room,
@@ -27,12 +36,17 @@ class Room extends Component {
         const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
         await ably.connection.once('connected');
         console.log('Connected to Ably!');
+        const channel = ably.channels.get('test');
+        await channel.subscribe('greeting', (message) => {
+            console.log('Received a greeting message in realtime: ' + message.data)
+        });
     }
 
     render() {
         return (
             <div>
-                Room: {this.state.room} // User: {this.state.user}
+                <div>Room: {this.state.room} // User: {this.state.user}</div><br/><br/><br/><br/>
+                <button onClick={this.onTestClick}>Test message</button>
             </div>
         );
     }
