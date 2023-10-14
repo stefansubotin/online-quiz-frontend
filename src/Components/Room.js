3c1f30033c7489d72276133e12c211a4f5fcb41e
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-14 14:29:23 +0200

    redo

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -1,4 +1,3 @@
-import { Pusher, PusherEvent } from '@pusher/pusher-websocket-react-native';
 import React, { Component } from 'react';
 
 class Room extends Component {
@@ -24,22 +23,7 @@ class Room extends Component {
     }
 
     async componentDidMount(){
-        const pusher = Pusher.getInstance();
-
-        await pusher.init({
-            apiKey: 'cefecd31795a4e419288',
-            cluster: 'eu'
-        });
-
-        let channel = await pusher.subscribe({
-            channelName: "my-channel",
-            onEvent: (event) => {
-                this.setState({
-                    room: event.eventName,
-                    user: event.data
-                })
-            }
-        })
+        
     }
 
     render(){
