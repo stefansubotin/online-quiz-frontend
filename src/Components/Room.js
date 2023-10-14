8e7056dcb0aaf91bcd44bdb9ab673094bddf1384
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-14 15:04:37 +0200

    ups

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -23,6 +23,7 @@ class Room extends Component {
     }
 
     async componentDidMount() {
+        const Ably = require('ably');
         const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
         await ably.connection.once('connected');
         console.log('Connected to Ably!');
