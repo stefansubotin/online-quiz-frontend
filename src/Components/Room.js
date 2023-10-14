adb230fe80b609db6660cd1b994ce9d6ac8ffe0f
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-14 15:02:23 +0200

    testing ably

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -23,19 +23,9 @@ class Room extends Component {
     }
 
     async componentDidMount() {
-        const Pusher = require("pusher");
-
-        const pusher = new Pusher({
-            appId: "1679697",
-            key: "cefecd31795a4e419288",
-            secret: "69192cf5d43c8f457530",
-            cluster: "eu",
-            useTLS: true
-        });
-
-        pusher.trigger("my-channel", "my-event", {
-            message: "hello world"
-        });
+        const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
+        await ably.connection.once('connected');
+        console.log('Connected to Ably!');
     }
 
     render() {
