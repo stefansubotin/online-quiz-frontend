e4e7955fc294949d5cf4569ead899444dfd05195
Osha <wolf.lena.0001@gmail.com>
2023-10-16 21:53:00 +0200

    added props

--- src/Components/Domino/Domino.js
+++ src/Components/Domino/Domino.js
@@ -1,3 +1,12 @@
 class Domino extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentComponent: 'domino',
+            room: '', 
+            user: '',
+            leader: false
+        };
+    }
     render() {return (<div>Hallo domino</div>)}
 } export default Domino;
