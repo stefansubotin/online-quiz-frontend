d5e1a2a288146064020fbcc302ce0d445545c471
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-18 16:23:36 +0200

    test

--- src/Components/Lobby.js
+++ src/Components/Lobby.js
@@ -29,8 +29,8 @@ class Lobby extends Component {
         let url = BackendAccess.getUrlKreuzwort();
         fetch(url, {
             method: 'POST',
-            body: JSON.stringify(body),
-            headers: { 'Content-Type': 'text/plain' }
+            body: body,
+            headers: { 'Content-Type': 'application/json' }
         }).then(response => response.json)
         .then(data => console.log(data))
         .catch(error => console.log(error));
