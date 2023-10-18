de418375d110df1a9dd14b3eb05a75bbc69b0882
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-18 17:43:14 +0200

    hmm

--- src/Components/Kreuzwort/Kreuzwort.js
+++ src/Components/Kreuzwort/Kreuzwort.js
@@ -89,7 +89,9 @@ class Kreuzwort extends Component {
 
 //#region OnEvent-Functions
     async onChangeLine(event) {
-        const ably = await this.getAbly();
+        const Ably = require('ably');
+        const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
+        await ably.connection.once('connected');
         const channelId = 'kreuzwort' + this.state.room;
         const channel = ably.channels.get(channelId);
         let eventIdArray = event.target.name.split('_');
