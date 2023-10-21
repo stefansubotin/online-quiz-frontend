a885c118a465956ca589596aa9ac8cb37fe4f4b0
Osha <wolf.lena.0001@gmail.com>
2023-10-21 15:49:13 +0200

    new error

--- src/Components/Player/Lobby.js
+++ src/Components/Player/Lobby.js
@@ -56,7 +56,7 @@ class Lobby extends Component {
     })
       .then((response) => response.json)
       .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log("Error" + error));
   }
 
   async onJoin(message) {
