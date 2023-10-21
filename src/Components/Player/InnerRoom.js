1a18547966df14ae6a860ede8a0a89e2a53b600d
Osha <wolf.lena.0001@gmail.com>
2023-10-21 21:17:53 +0200

    sad

--- src/Components/Player/InnerRoom.js
+++ src/Components/Player/InnerRoom.js
@@ -70,7 +70,7 @@ class InnerRoom extends Component {
           room: this.state.room,
           user: this.state.user,
           leader: this.state.leader,
-          data: dat,
+          data: JSON.parse(dat),
           currentComponent: "error",
         });
         break;
