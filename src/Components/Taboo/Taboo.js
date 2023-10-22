6265b77133f903847a8a02b9f82457ac0cd86089
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-22 15:38:20 +0200

    debug

--- src/Components/Taboo/Taboo.js
+++ src/Components/Taboo/Taboo.js
@@ -209,6 +209,7 @@ class Taboo extends Component {
     }
 
     async componentDidMount() {
+        console.log(JSON.parse(data));
         const Ably = require('ably');
         const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
         await ably.connection.once('connected');
