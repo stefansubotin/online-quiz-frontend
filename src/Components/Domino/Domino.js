33774dee298074ca33f571ae0fbd5901e5c8d7d3
Osha <wolf.lena.0001@gmail.com>
2023-10-22 11:18:50 +0200

    frage one

--- src/Components/Domino/Domino.js
+++ src/Components/Domino/Domino.js
@@ -9,7 +9,7 @@ class Domino extends Component {
       data: props.data,
       leader: false,
       pool: [],
-      feld: [],
+      feld: [{}],
     };
   }
   handleDragStart(e, id) {
@@ -27,40 +27,53 @@ class Domino extends Component {
     console.log(dat);
     let fragen = dat.fragen[0]
     console.log(fragen)
-    let frage = JSON.stringify(dat.fragen[0].props.frage)
+    let frage = dat.fragen[0].props.frage
     console.log("FRAge"+ frage );
     let antwort="Antwort"
-    let frages="Antwort"
+    let frages="frage"
     return(
     <div className="card" draggable="true">
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">{frages}</li>
+        <li className="list-group-item">{frage}</li>
         <li className="list-group-item">{antwort}</li>
       </ul>
     </div>);
   }
 
   initFeld() {
-    let newFeld=[];
+    let newFeld=[{}];
     for(let i= 0;i<9;++i){
-      newFeld.push({"id":i,"stein":null})
+      newFeld.push({"id":i,"stein":null, 'text': "hallo"})
     }
-    let snFeld = JSON.stringify(newFeld);
+
     this.state = {
       room: this.state.room,
       user: this.state.user,
       data: this.state.data,
       leader: false,
       pool: [],
-      feld: snFeld,
+      feld: newFeld,
     };
+  }
+  async componentDidMount(){
+    //Connection Ably to transfer and update Data
+    const Ably = require('ably');
+    const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
+    await ably.connection.once('connected');
+    const channelId = 'domino'+this.state.room;
+    const channel = ably.channels.get(channelId);
+    this.initFeld();
+
+
   }
 
   render() {
     return (
       <div>
         <div name="domino" className="dominoFeld">
-        
+          {this.state.feld.map((zelle, index)=>{
+            <div >Hallo</div>
+          })}
         </div>
         <div className="Pool card">{this.getOneCard()}</div>
       </div>
