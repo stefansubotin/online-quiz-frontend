039eda95ee2a74632f3c6772cea5f24ff7a8e7de
Osha <wolf.lena.0001@gmail.com>
2023-10-25 19:01:03 +0200

    fix

--- src/Components/Domino/Domino.js
+++ src/Components/Domino/Domino.js
@@ -481,7 +481,7 @@ class Domino extends Component {
     await ably.connection.once('connected');
     const channelId = 'domino' + this.state.room;
     const channel = ably.channels.get(channelId);
-    let dat = data
+    let dat = data.props
 
     let cAnswers = dat.correctAnswers
     let wAnswers = dat.wrongAnswers
