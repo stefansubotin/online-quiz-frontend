498f9efc7173fc222a9ed5f31a157c9d77c3e5a1
Stefan Subotin <stefanasubotin@gmail.com>
2023-11-19 13:31:53 +0100

    reset

--- src/Tools/BackendAccess.js
+++ src/Tools/BackendAccess.js
@@ -1,27 +1,35 @@
 class BackendAccess {
-  static getUrl() {
-    return "https://gifted-shawl-fox.cyclic.app/";
-  }
+    static getUrl() {
+        return "https://gifted-shawl-fox.cyclic.app/";
+    }
 
-  static getUrlKreuzwort() {
-    return this.getUrl() + "kreuzwort";
-  }
+    static getUrlKreuzwort() {
+        return this.getUrl() + "kreuzwort";
+    }
 
-  static getUrlWwm() {
-    return this.getUrl() + "wwm";
-  }
+    static getUrlWwm() {
+        return this.getUrl() + "wwm";
+    }
 
-  static getUrlContributor() {
-    return this.getUrl() + "contributor";
-  }
+    static getUrlContributor() {
+        return this.getUrl() + "contributor";
+    }
 
-  static getUrlDomino() {
-    return this.getUrl() + "domino";
-  }
+    static getUrlDomino() {
+        return this.getUrl() + "domino";
+    }
 
-  static getUrlTaboo() {
-    return this.getUrl() + "taboo";
-  }
+    static getUrlTaboo() {
+        return this.getUrl() + "taboo";
+    }
+
+    static getUrlLobby() {
+        return this.getUrl() + "lobby";
+    }
+
+    static getUrlLeaveLobby() {
+        return this.getUrl() + "db/"
+    }
 }
 
 export default BackendAccess;
