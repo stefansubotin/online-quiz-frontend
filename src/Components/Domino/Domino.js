1879f5f15abfb3b48383b5a3f1d6bc63a1a236e5
Stefan Subotin <stefanasubotin@gmail.com>
2023-11-19 13:45:34 +0100

    test

--- src/Components/Domino/Domino.js
+++ src/Components/Domino/Domino.js
@@ -6,7 +6,7 @@ import BackendAccess from "../../Tools/BackendAccess";
 class Domino extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = { 
       room: props.room,
       users: props.users,
       user: props.user,
